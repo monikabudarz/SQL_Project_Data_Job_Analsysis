118089cbad0bb41c7410566bfc1a2f1c19af27b7
monikabudarz <monikabudarz2@gmail.com>
2025-03-04 21:01:56 +0100

    upload sql files

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,55 @@
+/*
+optimal skills - high demand and high paying skills
+*/
+
+WITH skills_demand AS(
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    count(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+GROUP BY 
+    skills_dim.skill_id
+), average_salary AS (
+    SELECT 
+    skills_job_dim.skill_id,
+    round(avg(salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = TRUE
+    GROUP BY 
+        skills_job_dim.skill_id
+)
+-- rewriting this same query more concisely 
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT (skills_job_dim.job_id) AS demand_count,
+    round (avg(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+GROUP BY
+    skills_dim.skill_id
+HAVING 
+    COUNT(skills_job_dim.job_id) >10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+    
+limit 25;
